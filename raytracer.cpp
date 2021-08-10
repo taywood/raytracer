b06ce1c0923de329432bb316afbf4045c4ab5350
taywood <73618445+taywood@users.noreply.github.com>
2021-08-10 10:31:34 +0100

    Add files via upload

--- raytracer.cpp
+++ raytracer.cpp
@@ -132,78 +132,79 @@ void clamp255(Vec3f& col) {
     col.z = (col.z > 255) ? 255 : (col.z < 0) ? 0 : col.z;
 }
 
+void LineRender(SDL_Surface* screen, hittable_list world, int y, int spp, int max_depth, camera* cam) {
+    const float aspect_ratio = 16.0 / 9;
+    const int image_width = screen->w;
+    const int image_height = static_cast<int>(image_width / aspect_ratio);
+
+    const Colour black(0, 0, 0);
+    Colour pix_col(black);
+
+    for (int x = 0; x < screen->w; ++x) {
+        pix_col = black;
+        for (int s = 0; s < spp; s++) {
+            auto u = double(x + random_double()) / (image_width - 1);
+            auto v = double(y + random_double()) / (image_height - 1);
+            ray ray = cam->get_ray(u, v);
+            pix_col = pix_col + ray_colour(ray, world, max_depth);
+        }
+        pix_col /= 255.f * spp;
+        pix_col.x = sqrt(pix_col.x);
+        pix_col.y = sqrt(pix_col.y);
+        pix_col.z = sqrt(pix_col.z);
+        pix_col *= 255;
+        Uint32 colour = SDL_MapRGB(screen->format, pix_col.x, pix_col.y, pix_col.z);
+        putpixel(screen, x, y, colour);
+    }
+}
+
 // LAB 11 //
 
-    // Image
-const auto aspect_ratio = 16.0 / 9.0;
-const int image_width = screen->w;
-const int image_height = static_cast<int>(image_width / aspect_ratio);
-const int spp = 4;
-const float scale = 1.f / spp;
-const int max_depth = 50;
 
-    hittable_list random_scene() {
-        hittable_list world;
-        auto ground_material = make_shared<lambertian>(Colour(0.5, 0.5, 0.5));
-        world.add(make_shared<sphere>(Point3f(0, -1000, 0), 1000, ground_material));
-        for (int a = -11; a < 11; a++) {
-            for (int b = -11; b < 11; b++) {
-                auto choose_mat = random_double();
-                Point3f centre(a + 0.9 * random_double(), 0.2, b + 0.9 * random_double());
-                if ((centre - Point3f(4, 0.2, 0)).length() > 0.9) {
-                    shared_ptr<material> sphere_material;
-                    if (choose_mat < 0.8) {
-                      // diffuse
-                        auto albedo = Colour::random() * Colour::random();
-                        sphere_material = make_shared<lambertian>(albedo);
-                        world.add(make_shared<sphere>(centre, 0.2, sphere_material));
-                    }
-                    else if (choose_mat < 0.95) {
-                      // metal
-                        auto albedo = Colour::random(0.5, 1);
-                        auto fuzz = random_double(0, 0.5);
-                        sphere_material = make_shared<metal>(albedo, fuzz);
-                        world.add(make_shared<sphere>(centre, 0.2, sphere_material));
-                    }
-                    else {
-                      //glass
-                        sphere_material = make_shared<dielectric>(1.5);
-                        world.add(make_shared<sphere>(centre, 0.2, sphere_material));
-                    }
+hittable_list random_scene() {
+    hittable_list world;
+    auto ground_material = make_shared<lambertian>(Colour(0.5, 0.5, 0.5));
+    world.add(make_shared<sphere>(Point3f(0, -1000, 0), 1000, ground_material));
+    for (int a = -11; a < 11; a++) {
+        for (int b = -11; b < 11; b++) {
+            auto choose_mat = random_double();
+            Point3f centre(a + 0.9 * random_double(), 0.2, b + 0.9 * random_double());
+            if ((centre - Point3f(4, 0.2, 0)).length() > 0.9) {
+                shared_ptr<material> sphere_material;
+                if (choose_mat < 0.8) {
+                    // diffuse
+                    auto albedo = Colour::random() * Colour::random();
+                    sphere_material = make_shared<lambertian>(albedo);
+                    world.add(make_shared<sphere>(centre, 0.2, sphere_material));
+                }
+                else if (choose_mat < 0.95) {
+                    // metal
+                    auto albedo = Colour::random(0.5, 1);
+                    auto fuzz = random_double(0, 0.5);
+                    sphere_material = make_shared<metal>(albedo, fuzz);
+                    world.add(make_shared<sphere>(centre, 0.2, sphere_material));
+                }
+                else {
+                    //glass
+                    sphere_material = make_shared<dielectric>(1.5);
+                    world.add(make_shared<sphere>(centre, 0.2, sphere_material));
                 }
             }
-
         }
 
-        auto material1 = make_shared<dielectric>(1.5);
-        world.add(make_shared<sphere>(Point3f(0, 1, 0), 1.0, material1));
-        auto material2 = make_shared<lambertian>(Colour(0.4, 0.2, 0.1));
-        world.add(make_shared<sphere>(Point3f(-4, 1, 0), 1.0, material2));
-        auto material3 = make_shared<metal>(Colour(0.7, 0.6, 0.5), 0.0);
-        world.add(make_shared<sphere>(Point3f(4, 1, 0), 1.0, material3));
-        return world;
     }
 
-    // World
-    auto world = random_scene();
+    auto material1 = make_shared<dielectric>(1.5);
+    world.add(make_shared<sphere>(Point3f(0, 1, 0), 1.0, material1));
+    auto material2 = make_shared<lambertian>(Colour(0.4, 0.2, 0.1));
+    world.add(make_shared<sphere>(Point3f(-4, 1, 0), 1.0, material2));
+    auto material3 = make_shared<metal>(Colour(0.7, 0.6, 0.5), 0.0);
+    world.add(make_shared<sphere>(Point3f(4, 1, 0), 1.0, material3));
+    return world;
+}
 
-  // Camera
-    Point3f lookfrom(13, 2, 3);
-    Point3f lookat(0, 0, 0);
-    Vec3f vup(0, 1, 0);
-    auto dist_to_focus = 10.0;
-    auto aperture = 0.15;
-    camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus);
+// LAB 11 //
 
-    auto viewport_height = 2.0;
-    auto viewport_width = aspect_ratio * viewport_height;
-    auto focal_length = 1.0;
-    auto origin = Point3f(0, 0, 0);
-    auto horizontal = Vec3f(viewport_width, 0, 0);
-    auto vertical = Vec3f(0, viewport_height, 0);
-    auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - Vec3f(0, 0, focal_length);
-
-    // LAB 11 //
 
 int main(int argc, char** argv)
 {
@@ -243,6 +244,33 @@ int main(int argc, char** argv)
         //auto dist_to_focus = (lookfrom - lookat).length();
         //auto aperture = 2.0;
         //camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus);
+
+    // World
+    auto world = random_scene();
+
+    // Image
+    const auto aspect_ratio = 16.0 / 9.0;
+    const int image_width = screen->w;
+    const int image_height = static_cast<int>(image_width / aspect_ratio);
+    const int spp = 4;
+    const float scale = 1.f / spp;
+    const int max_depth = 50;
+
+    // Camera
+    Point3f lookfrom(13, 2, 3);
+    Point3f lookat(0, 0, 0);
+    Vec3f vup(0, 1, 0);
+    auto dist_to_focus = 10.0;
+    auto aperture = 0.15;
+    camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus);
+
+    auto viewport_height = 2.0;
+    auto viewport_width = aspect_ratio * viewport_height;
+    auto focal_length = 1.0;
+    auto origin = Point3f(0, 0, 0);
+    auto horizontal = Vec3f(viewport_width, 0, 0);
+    auto vertical = Vec3f(0, viewport_height, 0);
+    auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - Vec3f(0, 0, focal_length);
 
     double t;
     Colour pix_col(black);
@@ -322,3 +350,5 @@ int main(int argc, char** argv)
 
     return 0;
 }
+
+
